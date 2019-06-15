3e17f7bcce1eace0fd1dbd6dcb2d0604aa6eb9d2
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-06-15 13:33:20 +0200

    Updates

---
+++ precache-manifest.196a4232a46eb5eb19e092b4bf1e2e57.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b341837959162e0446a63dd90b6ee9",
+    "url": "/pressy-admin/index.html"
+  },
+  {
+    "revision": "04113cdf0346366886e7",
+    "url": "/pressy-admin/static/js/2.10cc9c84.chunk.js"
+  },
+  {
+    "revision": "3830c9a617134d0437e9",
+    "url": "/pressy-admin/static/js/main.ec0bd6ef.chunk.js"
+  },
+  {
+    "revision": "e87487f33e38a39748a6",
+    "url": "/pressy-admin/static/js/runtime~main.dc289da3.js"
+  }
+]);
